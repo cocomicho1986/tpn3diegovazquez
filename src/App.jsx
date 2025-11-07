9cd67d5ff4e3beb80074b2682fbf22bb99ce6e09
cocomicho1986 <diegovazquezlavaska@gmail.com>
2025-11-07 08:16:38 -0300

    comentarios App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,18 @@ export default function App() {
   // ✅ Inicializa el tema global al cargar la app
   const initializeTheme = useThemeStore(state => state.initializeTheme);
   
+  {/*          
+    A continuación, se detalla su funcionamiento:
+
+    React.useEffect(...): Este Hook de React se utiliza para manejar "efectos secundarios" (operaciones que interactúan con el mundo exterior, como la inicialización del tema, llamadas a API, o manipulación directa del DOM) en componentes funcionales.
+    () => { initializeTheme(); }: Es la función que contiene la lógica del efecto. En este caso, simplemente llama a otra función llamada initializeTheme para configurar el tema de la aplicación.
+    [initializeTheme]: Este es el array de dependencias. React monitoriza los valores dentro de este array entre renderizaciones.
+        Cuando el componente se renderiza inicialmente, el efecto se ejecuta.
+        En renderizaciones posteriores, React compara la referencia de la función initializeTheme con la que tenía en la renderización anterior.
+        Si la función initializeTheme es la misma que antes (misma referencia), el efecto no se vuelve a ejecutar.
+        Si la función initializeTheme es diferente (nueva referencia), el efecto se vuelve a ejecutar.
+    */}
+
   React.useEffect(() => {
     initializeTheme();
   }, [initializeTheme]);
